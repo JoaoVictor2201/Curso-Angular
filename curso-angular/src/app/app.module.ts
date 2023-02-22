c18f35cb7d9fd89d3c54292cb55eab3d615ef3f2
João Victor França <franca.jv@hotmail.com>
2023-02-22 16:47:30 -0300

    primeiro componente

--- curso-angular/src/app/app.module.ts
+++ curso-angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimeiroComponentComponent } from './components/primeiro-component/primeiro-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponentComponent
   ],
   imports: [
     BrowserModule
